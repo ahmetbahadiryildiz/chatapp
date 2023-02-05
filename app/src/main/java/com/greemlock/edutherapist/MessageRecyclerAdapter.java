edde94efb5a9f2582f8f1852beaf435ff33b7d82
ahmetbahadiryildiz <ivapp.greemlock@gmail.com>
2023-02-05 20:30:51 +0300

    version 1.0.5

--- app/src/main/java/com/greemlock/edutherapist/MessageRecyclerAdapter.java
+++ app/src/main/java/com/greemlock/edutherapist/MessageRecyclerAdapter.java
@@ -4,63 +4,108 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+public class MessageRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+    private List<ObjectMessage> messages;
 
-public class MessageRecyclerAdapter extends ArrayAdapter<ObjectMessage>{
-
-    private Context contextActivity;
-    private int otherMessageResource;
-
-    private static class ViewHolder {
-
-        TextView name;
-        TextView message;
-        TextView date;
-        ImageView imageView;
+    public MessageRecyclerAdapter(Context context, List<ObjectMessage> messages){
+        this.context = context;
+        this.messages = messages;
     }
 
-    public MessageRecyclerAdapter(Context context, int resourceOtherMessages, ArrayList<ObjectMessage> objects) {
-        super(context, resourceOtherMessages, objects);
-        contextActivity = context;
-        otherMessageResource = resourceOtherMessages;
+    public class ViewHolder0 extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView message;
+        public TextView date;
+        public ImageView imageView;
+
+        public ViewHolder0(@NonNull View itemView){
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            message = itemView.findViewById(R.id.message);
+            date = itemView.findViewById(R.id.date);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
     }
 
-    public View getView(int position, View convertView, ViewGroup parent){
+    public class ViewHolder1 extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView message;
+        public TextView date;
+        public ImageView imageView;
 
-        String name = getItem(position).getMessage_name();
-        String message = getItem(position).getMessage();
-        String date = getItem(position).getMessage_date();
-
-        ViewHolder holder;
-
-        if(convertView == null){
-            LayoutInflater inflater = LayoutInflater.from(contextActivity);
-            convertView = inflater.inflate(otherMessageResource, parent, false);
-
-
-            holder = new ViewHolder();
+        public ViewHolder1(@NonNull View itemView){
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            message = itemView.findViewById(R.id.message);
+            date = itemView.findViewById(R.id.date);
+            imageView = itemView.findViewById(R.id.imageView);
         }
-        else {
-            holder = (ViewHolder) convertView.getTag();
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View itemView;
+        switch (viewType) {
+            case 0:
+                itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.message_layout_recycler,parent,false);
+                return new ViewHolder0(itemView);
+            case 1:
+                itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.message_layout_recycler_my_messages,parent,false);
+                return new ViewHolder1(itemView);
         }
-        holder.name = convertView.findViewById(R.id.name);
-        holder.message = convertView.findViewById(R.id.message);
-        holder.date = convertView.findViewById(R.id.date);
-        holder.imageView = convertView.findViewById(R.id.imageView);
+        return null;
+    }
 
-        convertView.setTag(holder);
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        final ObjectMessage objectMessage = messages.get(position);
 
-        holder.name.setText(name);
-        holder.message.setText(message);
-        holder.date.setText(date);
-        holder.imageView.setImageResource(R.drawable.ic_baseline_person_24);
+        switch (holder.getItemViewType()) {
+            case 0:
+                ViewHolder0 viewHolder0 = (ViewHolder0)holder;
+                viewHolder0.name.setText(objectMessage.getMessage_name());
+                viewHolder0.message.setText(objectMessage.getMessage());
+                viewHolder0.date.setText(objectMessage.getMessage_date());
+                viewHolder0.imageView.setImageResource(R.drawable.ic_baseline_person_24);
+                break;
 
-        return convertView;
+            case 1:
+                ViewHolder1 viewHolder1 = (ViewHolder1)holder;
+                viewHolder1.name.setText(objectMessage.getMessage_name());
+                viewHolder1.message.setText(objectMessage.getMessage());
+                viewHolder1.date.setText(objectMessage.getMessage_date());
+                viewHolder1.imageView.setImageResource(R.drawable.ic_baseline_person_24);
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        final ObjectMessage objectMessage = messages.get(position);
+        String username = SaveSharedPreferences.getPrefName(context.getApplicationContext());
+
+        if(username.equals(objectMessage.getMessage_name())){
+
+            return 1;
+        }
+        else{
+            return 0;
+        }
     }
 
 }
